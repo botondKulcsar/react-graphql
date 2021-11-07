7baf21e4007758524dfb60c942d168de3eac4c19
Botond Kulcsar <bkulcsar@gmx.com>
2021-11-07 18:06:47 +0200

    refactor exercise 8.20 to use lazyQuery and NO state

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { useQuery } from '@apollo/client'
+import React, { useEffect } from 'react'
+import { useQuery, useLazyQuery } from '@apollo/client'
 import { ME, ALL_BOOKS } from '../queries'
 
 const Recommended = ({ show }) => {
-    const { loading, data } = useQuery(ME)
-    const [genre, setGenre] = useState(null)
-    
-    // const { data: { allBooks: books } = {} } = useQuery(ALL_BOOKS, { variables: { genre } })
-    const result = useQuery(ALL_BOOKS, { variables: { genre } })
-    
+    const { loading, data, refetch } = useQuery(ME)
+   
+    const [filterBooks, result] = useLazyQuery(ALL_BOOKS)
+
     useEffect(() => {
-       
-        setGenre(data?.me?.favoriteGenre)
-    }, [result.data]) //eslint-disable-line
+        if (!data || !data?.me || !data?.me?.favoriteGenre) {
+            refetch()
+            console.log('data: ', data)
+        }
+        if (show) {
+            filterBooks({ variables: { genre: data?.me?.favoriteGenre } })
+            console.log('filterBooks run')
+        }
+
+    }, [data, show]) //eslint-disable-line
     
+
     if (!show) {
         return null
     }
@@ -27,27 +33,27 @@ const Recommended = ({ show }) => {
     return (
         <div>
             <h2>recommendations</h2>
-            <p>books in your favorite genre <strong>{genre}</strong></p>
+            <p>books in your favorite genre <strong>{data?.me?.favoriteGenre}</strong></p>
             <table>
-        <tbody>
-          <tr>
-            <th>title</th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {result.data.allBooks.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+                <tbody>
+                    <tr>
+                        <th>title</th>
+                        <th>
+                            author
+                        </th>
+                        <th>
+                            published
+                        </th>
+                    </tr>
+                    {result?.data?.allBooks.map(a =>
+                        <tr key={a.title}>
+                            <td>{a.title}</td>
+                            <td>{a.author.name}</td>
+                            <td>{a.published}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 
